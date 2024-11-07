4784c0179104b928dc05dfcdb99b4868f0d2f6e4
dantedemagistre <89210010+dantedemagistre@users.noreply.github.com>
2024-11-07 13:22:10 -0300

    Profile Card
    Profile Card

--- src/_mock/blog.js
+++ src/_mock/blog.js
@@ -1,38 +1,38 @@
+import axios from 'axios';
+import { sample } from 'lodash';
 import { faker } from '@faker-js/faker';
+import { Email } from '@mui/icons-material';
+import { useEffect, useState } from 'react';
 
 // ----------------------------------------------------------------------
 
-const POST_TITLES = [
-  'Whiteboard Templates By Industry Leaders',
-  'Tesla Cybertruck-inspired camper trailer for Tesla fans who can’t just wait for the truck!',
-  'Designify Agency Landing Page Design',
-  '✨What is Done is Done ✨',
-  'Fresh Prince',
-  'Six Socks Studio',
-  'vincenzo de cotiis’ crossing over showcases a research on contamination',
-  'Simple, Great Looking Animations in Your Project | Video Tutorial',
-  '40 Free Serif Fonts for Digital Designers',
-  'Examining the Evolution of the Typical Web Design Client',
-  'Katie Griffin loves making that homey art',
-  'The American Dream retold through mid-century railroad graphics',
-  'Illustration System Design',
-  'CarZio-Delivery Driver App SignIn/SignUp',
-  'How to create a client-serverless Jamstack app using Netlify, Gatsby and Fauna',
-  'Tylko Organise effortlessly -3D & Motion Design',
-  'RAYO ?? A expanded visual arts festival identity',
-  'Anthony Burrill and Wired mag’s Andrew Diprose discuss how they made January’s Change Everything cover',
-  'Inside the Mind of Samuel Day',
-  'Portfolio Review: Is This Portfolio Too Creative?',
-  'Akkers van Margraten',
-  'Gradient Ticket icon',
-  'Here’s a Dyson motorcycle concept that doesn’t ‘suck’!',
-  'How to Animate a SVG with border-image',
-];
+export const userProfile = {
+  id: 'user_id',
+  cover: '/assets/images/covers/cover_profile.jpg',
+  title: 'Mi Perfil',
+  createdAt: new Date(),
+  Email: '',
+
+  author: {
+    name: 'Nombre Usuario', 
+    avatarUrl: '/assets/images/avatars/avatar_user.jpg',
+  },
+};
+
+// Corregido: Asegúrate de retornar los datos
+export const fetchUserProfile = async () => {
+  try {
+    const response = await axios.get('http://localhost:3030/perfil'); 
+    return response.data[0]; // Devuelve el primer objeto del array (tu perfil)
+  } catch (error) {
+    console.error("Error al obtener el perfil del usuario:", error);
+    return null; // Si hay un error, retorna null
+  }
+};
 
 export const posts = [...Array(23)].map((_, index) => ({
   id: faker.string.uuid(),
   cover: `/assets/images/covers/cover_${index + 1}.jpg`,
-  title: POST_TITLES[index + 1],
   createdAt: faker.date.past(),
   view: faker.number.int(99999),
   comment: faker.number.int(99999),
