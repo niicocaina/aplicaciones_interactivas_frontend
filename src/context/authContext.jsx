6e403a911f0d59ff6b96797026ff047a68d1af88
ivan <43122250+iivigarcia@users.noreply.github.com>
2024-12-02 22:11:34 -0300

    preserva la sesion ahora

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { serviceLogin } from 'src/services/authService';
 import axios from 'axios';
@@ -6,10 +6,31 @@ import axios from 'axios';
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(); // Mantener el estado user
-  const [token, setToken] = useState(); // Agregar estado para el token
+
+  const [user, setUser] = useState(() => {
+    // Inicializar estado con datos persistidos
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const [token, setToken] = useState(() => {
+    // Inicializar estado con el token persistido
+    return localStorage.getItem('token') || null;
+  });
+
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Sincronizar `user` y `token` con `localStorage`
+    if (user && token) {
+      localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('token', token);
+    } else {
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+    }
+  }, [user, token]);
 
   const login = async (mail, password) => {
     try {
@@ -53,48 +74,3 @@ export function AuthProvider({ children }) {
 }
 
 export default AuthContext;
-
-/*import { Password } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
-import { serviceLogin } from 'src/services/authService';
-import { createContext, useState, Children } from 'react';
-
-const AuthContext = createContext();
-
-export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
-
-  const login = async (mail, password) => {
-    try {
-      const response = await serviceLogin(mail, password);
-      const userData = response;
-      if (userData.length === 0) {
-        setError('Usuario o contraseña incorrectos');
-        return;
-      }
-      setUser(userData[0]);
-      if (userData[0].role === 'ADMIN') {
-        navigate('/products');
-        return;
-      } else {
-        navigate('/catalogue');
-      }
-    } catch (err) {
-      setError('Ha ocurrido un error');
-    }
-  };
-
-  const logout = () => {
-    setUser(null);
-    navigate('/login');
-  };
-
-  return (
-    <AuthContext.Provider value={{ user, login, logout, error }}>{children}</AuthContext.Provider>
-  );
-}
-
-export default AuthContext;
-*/
